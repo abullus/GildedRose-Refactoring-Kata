1faee4f4162f21e031d79d090ff0c80e24fe4ce9
abullus <67292876+abullus@users.noreply.github.com>
2020-07-14 12:05:23 +0100

    Removed irrelevant tests

--- csharp/GildedRoseTest.cs
+++ csharp/GildedRoseTest.cs
@@ -7,15 +7,6 @@ namespace csharp
     public class GildedRoseTest
     {
         [Test]
-        public void foo()
-        {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
-            GildedRose app = new GildedRose(Items);
-            app.UpdateQuality();
-            Assert.AreEqual("fixme", Items[0].Name);
-        }
-
-        [Test]
         public void Sulfuras_Have_Constant_Quality()
         {
             IList<Item> Items = new List<Item> { new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80}};
